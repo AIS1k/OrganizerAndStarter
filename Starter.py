d6ca5bf8ac8d255e949e00314b13005a8f4a781b
AIS1k <morfly@inbox.ru>
2023-12-07 20:16:50 +0300

    Переделал конструкции if на dict

--- Starter.py
+++ Starter.py
@@ -1,81 +1,73 @@
 import os
 import time
+import logging
 from prettytable import PrettyTable
 from modPick import pick
 
 os.system('color 2')
+logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w")
 
-clear = lambda: os.system('cls')
+
+def clear():
+    os.system('cls')
+
 
 data = [
-	"блокнот", "C:/Windows/System32/notepad.exe",
-	"stalker", "C:/Users/AIS/Desktop/Anomaly Launcher.lnk",
-	"hunter", "D:/Games/Silent Hunter 5/sh5.exe",
-	"mo2", "F:/Games/MO2/ModOrganizer.exe",
-	"total", "D:/totalcmd/TOTALCMD64.EXE",
-	"шахматы", "D:/Games/LucasChessR/bin/LucasR.exe",
-	"браузер", "C:/Program Files/Mozilla Firefox/firefox.exe",
-	"таблица", "no path"
+    "блокнот", "C:/Windows/System32/notepad.exe",
+    "stalker", "C:/Users/AIS/Desktop/Anomaly Launcher.lnk",
+    "hunter", "D:/Games/Silent Hunter 5/sh5.exe",
+    "mo2", "F:/Games/MO2/ModOrganizer.exe",
+    "total", "D:/totalcmd/TOTALCMD64.EXE",
+    "шахматы", "D:/Games/LucasChessR/bin/LucasR.exe",
+    "браузер", "C:/Program Files/Mozilla Firefox/firefox.exe",
+    "таблица", "no path"
 ]
 
+
 def show_table():
-	#	ТАБЛИЦА
-	columns = ["Команда", "Путь"]
-	count_col = len(columns)
-	table = PrettyTable(columns)
-	table.align = "l"
-	table.sortby = "Команда"
+    # ТАБЛИЦА
+    columns = ["Команда", "Путь"]
+    count_col = len(columns)
+    table = PrettyTable(columns)
+    table.align = "l"
+    table.sortby = "Команда"
 
-	td_data = data[:]
+    td_data = data[:]
 
-	while td_data:
-		table.add_row(td_data[:count_col])
-		td_data = td_data[count_col:]
-	print(table)
-
+    while td_data:
+        table.add_row(td_data[:count_col])
+        td_data = td_data[count_col:]
+    print(table)
 
 
 title = 'Запуск:'
 
-#	ОБРАБОТКА ИНПУТА ОТ ПОЛЬЗОВАТЕЛЯ
+logging.info("Запуск цикла")
 while True:
-	clear()
-	option, index = pick(data[::2], title, indicator='>', default_index=0)
-	userInput = option
-	execDone = False
+    clear()
+    option, index = pick(data[::2], title, indicator='>', default_index=0)
+    userInput = option
+    execDone = False
 
-	if userInput == "таблица":
-		show_table()
-		time.sleep(5)
-		continue
-		
-	for ind, elem in enumerate(data):
-		if userInput == elem:
-			command = data[ind + 1]
-			try:
-				os.startfile(command)
-				execDone = True
-			except:
-				print("Error: no path")
-			break
+    # ОБРАБОТКА ИНПУТА ОТ ПОЛЬЗОВАТЕЛЯ
+    if userInput == "таблица":
+        show_table()
+        time.sleep(5)
+        continue
 
-	if execDone:
-		print(userInput + " >>> выполнено")
-	else:
-		print(userInput + " - программа не обнаружена")
-	time.sleep(2)
+    for ind, elem in enumerate(data):
+        if userInput == elem:
+            command = data[ind + 1]
+            try:
+                os.startfile(command)
+                execDone = True
+                logging.info("Запуск  " + command)
+            except FileNotFoundError:
+                logging.info("Ошибка запуска  " + command)
+            break
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if execDone:
+        print(userInput + " >>> выполнено")
+    else:
+        print(userInput + " - программа не обнаружена")
+    time.sleep(2)
--- Tasks.py
+++ Tasks.py
@@ -1,8 +1,9 @@
-# from pick import pick
+# OrganizerAndStarter
+# Для работы через IDE (PyCharm) необходимо включить эмуляцию терминала в конфиге проекта
+
 from modPick import pick
 import datetime
 import os
-from os.path import abspath
 import csv
 import time
 
@@ -10,8 +11,10 @@ os.system('color 6')
 
 clear = lambda: os.system('cls')
 
+
 def show_pick(title, options, index):
-	return pick(options, title, indicator='>', default_index=index)
+    return pick(options, title, indicator='>', default_index=index)
+
 
 fileName = 'YourTasks.csv'
 mine_title = 'Главное меню:'
@@ -23,114 +26,117 @@ create_str = "Создать"
 remove_str = "Удалить"
 save_str = "Сохранить"
 cancel_str = "Отмена"
+exit_str = "Выход"
 
-mine_options = [tasks_str, idea_str, notes_str, save_str] #  пункты - Главное меню
-base_second_menu = [remove_str, create_str, back_str] #  пункты - Второстепенное меню
+mine_options = [tasks_str, idea_str, notes_str, save_str, exit_str]  # пункты - Главное меню
+base_second_menu = [remove_str, create_str, back_str]  # пункты - Второстепенное меню
 
-dict_type = {}
-dict_type[tasks_str] = []
-dict_type[idea_str] = []
-dict_type[notes_str] = []
+dict_type = {tasks_str: [], idea_str: [], notes_str: []}
+
+
+def fnc_exit():
+    print("Завершение работы")
+    time.sleep(1)
+    exit()
+
+
+def fnc_back():
+    _index = mine_options.index(title)
+    _title = mine_title
+    _options = mine_options
+    return _title, _options, _index
+
+
+def fnc_tasks():
+    _title = tasks_str
+    _options = dict_type[_title]
+    _index = 0
+    return _title, _options, _index
+
+
+def fnc_idea():
+    _title = idea_str
+    _options = dict_type[_title]
+    _index = 0
+    return _title, _options, _index
+
+
+def fnc_notes():
+    _title = notes_str
+    _options = dict_type[_title]
+    _index = 0
+    return _title, _options, _index
+
+
+def fnc_create():
+    _title = title
+    _options = dict_type[_title]
+    _index = base_second_menu.index(option)
+    user_input = input("создание " + _title + "\n>>> ")
+    if user_input != "":
+        current_date = datetime.datetime.now()
+        _options.append(current_date.strftime("%Y-%m-%d %H:%M") + " - " + user_input)
+    return _title, _options, _index
+
+
+def fnc_remove():
+    _title = title
+    _options = dict_type[_title]
+    _index = base_second_menu[::-1].index(remove_str)
+    try:
+        #  Отмена удаляет сама себя, затем добавляется снова
+        selected, ind = pick([cancel_str] + dict_type[title][3:], remove_str, indicator='->', default_index=0)
+        dict_type[title].remove(selected)
+    except:
+        print("error")
+    return _title, _options, _index
+
+
+def fnc_save():
+    _title = mine_title
+    _options = mine_options
+    _index = mine_options.index(save_str)
+
+    with open(fileName, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows([
+            [tasks_str] + dict_type[tasks_str][3:],
+            [idea_str] + dict_type[idea_str][3:],
+            [notes_str] + dict_type[notes_str][3:]
+        ])
+    print("Сохранено успешно!")
+    time.sleep(1)
+    return _title, _options, _index
 
 
 #  Создание и обработка файла базы данных
-file = open(fileName,'a')
+file = open(fileName, 'a')
 file.close()
 with open(fileName, 'r') as f:
-	for row in csv.reader(f):
-		dict_type[row[0]] = row[1:]
+    for row in csv.reader(f):
+        dict_type[row[0]] = row[1:]
 
 #  Добавление пунктов Второстепенного меню к спискам
 for list in [dict_type[tasks_str], dict_type[idea_str], dict_type[notes_str]]:
-	for item in base_second_menu:
-		list.insert(0, item)
-		
+    for item in base_second_menu:
+        list.insert(0, item)
 
 # активация Стартовое меню
 title = mine_title
 options = mine_options
 
-
-
 index = 0
 while 1:
-	clear()
-	option, index = show_pick(title, options, index) #  Вывод меню на экран
-	
-	#  Обработка событий
-	if option == tasks_str:
-		title = tasks_str
-		options = dict_type[tasks_str]
-		index = 0
-		continue
+    clear()
+    option, currentInd = show_pick(title, options, index)  # Вывод меню на экран
 
-	elif option == idea_str:
-		title = idea_str
-		options = dict_type[idea_str]
-		index = 0
-		continue
-
-	elif option == notes_str:
-		title = notes_str
-		options = dict_type[notes_str]
-		index = 0
-		continue
-
-	elif option == back_str:
-		index = mine_options.index(title)
-		title = mine_title
-		options = mine_options
-		continue
-		
-	elif option == create_str:
-		user_input = input("создание " + title + "\n>>> ")
-		if user_input == "":
-			continue
-		current_date = datetime.datetime.now()
-		dict_type[title].append(current_date.strftime("%Y-%m-%d %H:%M") + " - " + user_input)
-		continue
-		
-	elif option == remove_str:
-		try:
-			#  Отмена удаляет сама себя, затем добавляется снова
-			selected, ind = pick([cancel_str] + dict_type[title][3:], remove_str, indicator='->', default_index=0)
-			dict_type[title].remove(selected)
-		except:
-			print("error")
-		continue
-	
-	elif option == save_str:
-		with open(fileName, 'w', newline='') as f:
-			writer = csv.writer(f)
-			writer.writerows([
-				[tasks_str] + dict_type[tasks_str][3:],
-				[idea_str] + dict_type[idea_str][3:],
-				[notes_str] + dict_type[notes_str][3:]
-			])
-		print("Сохранено успешно!")
-		time.sleep(1)
-	
-	time.sleep(1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    title, options, index = {
+        exit_str: fnc_exit,
+        tasks_str: fnc_tasks,
+        idea_str: fnc_idea,
+        notes_str: fnc_notes,
+        back_str: fnc_back,
+        create_str: fnc_create,
+        remove_str: fnc_remove,
+        save_str: fnc_save
+    }.get(option)()
